ff071d905bd93850957b4122595fa57dcfd2be8f
Thomas Ostendorf <thomas.ostendorf@orbitlabs.de>
2023-10-03 16:50:06 +0200

    Get connection to backend

--- app/page.tsx
+++ app/page.tsx
@@ -1,30 +1,60 @@
 "use client";
+import { getItems } from "@/lib/api";
 import {
   List,
   ListItem,
+  MenuItem,
+  Select,
   TextField,
   Typography,
 } from "@/node_modules/@mui/material/index";
-import { useState } from "react";
+import { ItemsResponse, WeatherType } from "@/pages/api/items";
+import { useEffect, useState } from "react";
 
-const data = {
-  items: [
-    {
-      id: "foodbar",
-      name: "foobar",
-    },
-    {
-      id: "baaz",
-      name: "baz",
-    },
-  ],
+type PacklisteState = {
+  [id: string]: number;
 };
 
-export default function Home() {
-  const [values, setValues] = useState<{ [id: string]: number }>({});
+export default function Packlist() {
+  const [values, setValues] = useState<PacklisteState>({});
+  const [data, setData] = useState<ItemsResponse>({ items: [] });
+  const [days, setDays] = useState<number>(0);
+  const [weatherType, setWeatherType] = useState(WeatherType.normal);
+  useEffect(() => {
+    getItems({ days: days, weatherType: weatherType }).then((data) => {
+      let tmp: PacklisteState = {};
+      data.items.forEach((item: any) => {
+        tmp[item.id] = item.defaultValue;
+      });
+      setValues(tmp);
+      setData(data);
+    });
+  }, [days, weatherType]);
+
+  const handleWeatherDataChange = (event: any) => {
+    setWeatherType(event.target.value);
+  };
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-start p-24">
       <Typography variant="h4">Packlist</Typography>
+      <Typography variant="h5"> Wie viele Tage verreist du?</Typography>
+      <TextField
+        type="number"
+        variant="standard"
+        onChange={(e: any) => setDays(e.target.value)}
+      />
+      <Typography variant="h5">Wie ist das Wetter?</Typography>
+      <Select
+        id="weather-type-select"
+        value={weatherType}
+        label="Weather Type"
+        onChange={handleWeatherDataChange}
+      >
+        <MenuItem value={WeatherType.cold}>Kalt</MenuItem>
+        <MenuItem value={WeatherType.normal}>Normal</MenuItem>
+        <MenuItem value={WeatherType.warm}>Warm</MenuItem>
+      </Select>
       <List dense className="w-60">
         {data.items.map((item) => {
           return (
--- pages/api/items.ts
+++ pages/api/items.ts
@@ -3,26 +3,45 @@ import type { NextApiRequest, NextApiResponse } from "next";
 type Item = {
   id: string;
   name: string;
+  defaultValue: number;
 };
 
-type ItemsResponse = {
-  items: [Item];
+export enum WeatherType {
+  warm,
+  normal,
+  cold,
+}
+
+export type ItemsResponse = {
+  items: Item[];
+};
+
+export type ItemsRequest = {
+  days: number;
+  weatherType: WeatherType;
 };
 
 export default function handler(
-  req: NextApiRequest,
+  req: NextApiRequest<ItemsRequest>,
   res: NextApiResponse<ItemsResponse>
 ) {
-  res.status(200).json({
-    items: [
-      {
-        id: "trousers",
-        name: "Hosen",
-      },
-      {
-        id: "t_shirts",
-        name: "T-Shirts",
-      },
-    ],
-  });
+  if (req.method === "POST") {
+    const body: ItemsRequest = req.body;
+    return res.status(200).json({
+      items: [
+        {
+          id: "trousers",
+          name: "Hosen",
+          defaultValue: 2,
+        },
+        {
+          id: "t_shirts",
+          name: "T-Shirts",
+          defaultValue: body.days * 1,
+        },
+      ],
+    });
+  } else {
+    throw Error("Unimplemented");
+  }
 }
